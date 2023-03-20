e249e31801f16ab14b59e69bc487a64726a146c8
Radin <lilrwdi@gmail.com>
2023-03-21 01:40:49 +0330

    starts here

---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  MaterialColor primaryBlack = const MaterialColor(
+    0xFF000000,
+    <int, Color>{
+      50: Color(0xFF000000),
+      100: Color(0xFF000000),
+      200: Color(0xFF000000),
+      300: Color(0xFF000000),
+      400: Color(0xFF000000),
+      500: Color(0xFF000000),
+      600: Color(0xFF000000),
+      700: Color(0xFF000000),
+      800: Color(0xFF000000),
+      900: Color(0xFF000000),
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Pws",
+      theme: ThemeData(
+        primarySwatch: primaryBlack,
+        primaryColor: Colors.black,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final textController = TextEditingController();
+  final searchController = TextEditingController();
+  final replaceController = TextEditingController();
+
+  bool _isReplacing = false;
+
+  @override
+  void dispose() {
+    // Clean up the controllers when the widget is disposed.
+    textController.dispose();
+    searchController.dispose();
+    replaceController.dispose();
+    super.dispose();
+  }
+
+  void _replaceText() {
+    final String text = textController.text;
+    final String search = searchController.text;
+    final String replace = replaceController.text;
+
+    if (text.isEmpty || search.isEmpty || replace.isEmpty) {
+      return;
+    }
+
+    setState(() {
+      _isReplacing = true;
+    });
+
+    final String newText = text.replaceAll(search, replace);
+
+    Future.delayed(const Duration(milliseconds: 400), () {
+      setState(() {
+        textController.text = newText;
+        _isReplacing = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: textController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter the text here',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: 8,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              TextField(
+                controller: searchController,
+                decoration: const InputDecoration(
+                  labelText: 'Which character do you want to replace?',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              TextField(
+                controller: replaceController,
+                decoration: const InputDecoration(
+                  focusedBorder: OutlineInputBorder(),
+                  labelText: 'Which character do you want to replace it with?',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              //change btn
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: _isReplacing ? null : _replaceText,
+                  child: _isReplacing
+                      ? const SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 3,
+                          ),
+                        )
+                      : const Text(
+                          'Replace',
+                          style: TextStyle(),
+                        ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+// rang ro trancparency kon bere kolan . yani kadr haro sefid kon ama kam rang kon
+
