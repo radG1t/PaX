241b338d88699a632ed17462dce29249e2743213
Radin <lilrwdi@gmail.com>
2023-03-25 09:44:22 +0330

    comments deleted.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-
   MaterialColor primaryBlack = const MaterialColor(
     0xFF000000,
     <int, Color>{
@@ -22,7 +21,6 @@ class MyApp extends StatelessWidget {
       900: Color(0xFF000000),
     },
   );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -43,45 +41,11 @@ class HomePage extends StatelessWidget {
   const HomePage({
     Key? key,
   }) : super(key: key);
-
-  // final textController = TextEditingController();
   @override
-  // void dispose() {
-  //   // Clean up the controllers when the widget is disposed.
-  //   textController.dispose();
-  //   searchController.dispose();
-  //   replaceController.dispose();
-  //   super.dispose();
-  // }
-
-  // void _replaceText() {
-  //   final String text = textController.text;
-  //   final String search = searchController.text;
-  //   final String replace = replaceController.text;
-
-  //   if (text.isEmpty || search.isEmpty || replace.isEmpty) {
-  //     return;
-  //   }
-
-  //   setState(() {
-  //     _isReplacing = true;
-  //   });
-
-  //   final String newText = text.replaceAll(search, replace);
-
-  //   Future.delayed(const Duration(milliseconds: 400), () {
-  //     setState(() {
-  //       textController.text = newText;
-  //       _isReplacing = false;
-  //     });
-  //   });
-  // }
-
 //ui
   @override
   Widget build(BuildContext context) {
     return const HomeUi();
   }
 }
-
 // responsive / copy final text / show keys / clear all
